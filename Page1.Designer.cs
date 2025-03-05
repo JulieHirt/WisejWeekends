5084294124db583de7d2cc8c266b7c62abe50616
JulieBodette <jr3python@gmail.com>
2025-03-05 15:58:31 -0500

    Added buttons for shooting bullets

--- Page1.Designer.cs
+++ Page1.Designer.cs
@@ -33,6 +33,12 @@
 			this.buttonRight = new Wisej.Web.Button();
 			this.buttonLeft = new Wisej.Web.Button();
 			this.buttonDown = new Wisej.Web.Button();
+			this.label1 = new Wisej.Web.Label();
+			this.label2 = new Wisej.Web.Label();
+			this.ShootUp = new Wisej.Web.Button();
+			this.ShootRight = new Wisej.Web.Button();
+			this.ShootLeft = new Wisej.Web.Button();
+			this.ShootDown = new Wisej.Web.Button();
 			((System.ComponentModel.ISupportInitialize)(this.GameCanvas)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -80,19 +86,76 @@
 			this.buttonDown.TabIndex = 6;
 			this.buttonDown.Click += new System.EventHandler(this.buttonDown_Click);
 			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(738, 102);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(36, 15);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "Move";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(985, 102);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(38, 15);
+			this.label2.TabIndex = 9;
+			this.label2.Text = "Shoot";
+			// 
+			// ShootUp
+			// 
+			this.ShootUp.ImageSource = "scrollbar-arrow-up";
+			this.ShootUp.Location = new System.Drawing.Point(985, 132);
+			this.ShootUp.Name = "ShootUp";
+			this.ShootUp.Size = new System.Drawing.Size(50, 50);
+			this.ShootUp.TabIndex = 10;
+			// 
+			// ShootRight
+			// 
+			this.ShootRight.ImageSource = "icon-last";
+			this.ShootRight.Location = new System.Drawing.Point(1035, 188);
+			this.ShootRight.Name = "ShootRight";
+			this.ShootRight.Size = new System.Drawing.Size(50, 50);
+			this.ShootRight.TabIndex = 11;
+			// 
+			// ShootLeft
+			// 
+			this.ShootLeft.ImageSource = "icon-first";
+			this.ShootLeft.Location = new System.Drawing.Point(937, 188);
+			this.ShootLeft.Name = "ShootLeft";
+			this.ShootLeft.Size = new System.Drawing.Size(50, 50);
+			this.ShootLeft.TabIndex = 12;
+			// 
+			// ShootDown
+			// 
+			this.ShootDown.ImageSource = "scrollbar-arrow-down";
+			this.ShootDown.Location = new System.Drawing.Point(985, 244);
+			this.ShootDown.Name = "ShootDown";
+			this.ShootDown.Size = new System.Drawing.Size(50, 50);
+			this.ShootDown.TabIndex = 13;
+			// 
 			// Page1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
 			this.AutoScaleMode = Wisej.Web.AutoScaleMode.Font;
+			this.Controls.Add(this.ShootDown);
+			this.Controls.Add(this.ShootLeft);
+			this.Controls.Add(this.ShootRight);
+			this.Controls.Add(this.ShootUp);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
 			this.Controls.Add(this.buttonDown);
 			this.Controls.Add(this.buttonLeft);
 			this.Controls.Add(this.buttonRight);
 			this.Controls.Add(this.buttonUp);
 			this.Controls.Add(this.GameCanvas);
 			this.Name = "Page1";
-			this.Size = new System.Drawing.Size(1230, 430);
+			this.Size = new System.Drawing.Size(1154, 594);
 			((System.ComponentModel.ISupportInitialize)(this.GameCanvas)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -103,6 +166,12 @@
 		private Wisej.Web.Button buttonRight;
 		private Wisej.Web.Button buttonLeft;
 		private Wisej.Web.Button buttonDown;
+		private Wisej.Web.Label label1;
+		private Wisej.Web.Label label2;
+		private Wisej.Web.Button ShootUp;
+		private Wisej.Web.Button ShootRight;
+		private Wisej.Web.Button ShootLeft;
+		private Wisej.Web.Button ShootDown;
 	}
 }
 
